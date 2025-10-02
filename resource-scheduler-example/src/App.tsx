04d81241c5f9ee2e2b4b673f0e5148f717e103a0
Mohammed Obaid <mdobaid311@gmail.com>
2025-10-02 10:59:24 +0530

    chore: add @vercel/analytics dependency and integrate Analytics component in App

--- resource-scheduler-example/src/App.tsx
+++ resource-scheduler-example/src/App.tsx
@@ -1,11 +1,13 @@
 import Scheduler from "./components/Scheduler";
 import "resource-scheduler/dist/resource-scheduler.css";
 import logo from "./assets/obaid-code-logo.svg";
+import { Analytics } from "@vercel/analytics/next";
 
 const App = () => {
   return (
     <div>
       <Scheduler />
+      <Analytics />
       <div className="flex flex-col items-center text-center text-gray-500 text-sm mt-8">
         <img src={logo} alt="obaid-code logo" className="w-16 h-16 mb-2" />
         <span>
